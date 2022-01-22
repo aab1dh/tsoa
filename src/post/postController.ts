0bff1a503979811832593dca641a3b0dd75107b1
Mir Abid Hussain <aabidhussain@gmail.com>
2022-01-22 09:57:51 -0500

    remove build

---
+++ src/post/postController.ts
@@ -0,0 +1,50 @@
+// src/users/usersController.ts
+import * as express from "express";
+import {
+    Body,
+    Controller,
+    Get,
+    Path,
+    Post,
+    Query,
+    Route,
+    Request,
+    Response,
+    SuccessResponse
+} from "tsoa";
+import { blogPost } from "./post";
+import { PostsService, PostCreationParams } from "./postService";
+
+interface ValidateErrorJSON {
+    message: "Validation failed";
+    details: { [name: string]: unknown };
+}
+
+interface NotFoundErrorJSON {
+    message: "Post not found";
+    details: { [name: string]: unknown };
+}
+
+@Route("post")
+export class PostsController extends Controller {
+    @Response<NotFoundErrorJSON>(404, "Post not found")
+    @Get("{postId}")
+    public async getPost(
+        @Path() postId: number,
+        @Query() name?: string
+    ) {
+        return new PostsService().get(postId, name);
+    }
+
+    @Response<ValidateErrorJSON>(422, "Validation Failed")
+    @SuccessResponse("201", "Created") // Custom success response
+    @Post()
+    public async createPost(
+        @Body() requestBody: PostCreationParams,
+        @Request() request?: express.Request
+    ): Promise<blogPost> {
+        this.setStatus(201); // set return status 201
+        console.log(request?.body)
+        return new PostsService().create(requestBody);;
+    }
+}
---
+++ src/post/postService.ts
@@ -0,0 +1,18 @@
+import { blogPost } from "./post";
+const JSONdb = require('simple-json-db');
+const db = new JSONdb('db.json');
+// A post request should not contain an id.
+export type PostCreationParams = Pick<blogPost, "email" | "name" | "phoneNumbers">;
+
+export class PostsService {
+    public get(id: number, name?: string): blogPost {
+        return db.get(id || name);
+    }
+
+    public create(userCreationParams: PostCreationParams): blogPost {
+        return db.set(Math.floor(Math.random() * 10000), {
+            status: "Happy",
+            ...userCreationParams,
+        });
+    }
+}
